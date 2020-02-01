a08c74a1e7ebf2d2d68923be532011cd1a94fba0
belousovromnik <belousov.romnik@yandex.ru>
2020-02-01 13:30:44 +0300

    about to deploy

--- my_site/settings/production.py
+++ my_site/settings/production.py
@@ -24,13 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = [
-    'sleepy-inlet-54680.herokuapp.com',
-    'lit-coast-26708.herokuapp.com'
-    ]
+ALLOWED_HOSTS = ['lit-coast-26708.herokuapp.com']
 
 # Application definition
 
