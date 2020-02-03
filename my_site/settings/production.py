f58bb1412e617b15627303757b164b6f915da12c
belousov.romnik@yandex.ru <belousov.romnik@yandex.ru>
2020-02-03 10:54:12 +0300

    about to deploy

--- my_site/settings/production.py
+++ my_site/settings/production.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['lit-coast-26708.herokuapp.com']
+ALLOWED_HOSTS = ['radiant-island-17744.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
